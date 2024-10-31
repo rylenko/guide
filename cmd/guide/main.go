b7d5392727c66ececf8b818b2db9d5b183f44f47
rylenko <rylenko@tuta.io>
2024-10-31 17:40:53 +0700

    Add todos, some corrections in cmd/guide

--- cmd/guide/main.go
+++ cmd/guide/main.go
@@ -9,11 +9,23 @@ import (
 	"github.com/rylenko/guide/internal/std_network"
 )
 
-const APIKey string = "baf019de-382e-4910-b1c4-906233282200"
+const GHAPIKey string = "baf019de-382e-4910-b1c4-906233282200"
 
 func main() {
-	geocoder := gh_geocode.NewGeocoder(std_network.NewRequester(), APIKey)
+	// Create a new instance of standard network requester.
+	requester := std_network.NewRequester()
 
+	// Create a new instance of graphhopper geocoder.
+	//
+	// TODO: Support language parameter.
+	geocoder := gh_geocode.NewGeocoder(requester, GHAPIKey)
+
+	// Create a new instance of openweathermap fetcher.
+	//
+	// TODO: Support language parameter.
+	// weatherFetcher := owm_weather.NewFetcher(requester, OWMAPIKey)
+
+	// Launch application using interface instances and standard IO.
 	if err := launch.Launch(geocoder, os.Stdin, os.Stdout); err != nil {
 		log.Fatal("Failed to launch: ", err)
 	}
